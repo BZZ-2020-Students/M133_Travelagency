bfc24a49fd8c045437eb1425707e94257e14943a
Loic190 <flueckigerl@bzz.ch>
2022-07-11 16:42:14 +0200

    token deleted

--- src/main/java/ch/bzz/reisebuero/util/AuthorizationFilter.java
+++ src/main/java/ch/bzz/reisebuero/util/AuthorizationFilter.java
@@ -7,81 +7,71 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ResourceInfo;
 import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Cookie;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
 
-/**
- * checks the authorization for the services
- * Source: https://howtodoinjava.com/jersey/jersey-rest-security/
- */
+
+import static jakarta.ws.rs.core.Response.Status.UNAUTHORIZED;
+
 
 @Provider
 public class AuthorizationFilter implements ContainerRequestFilter {
-
     @Context
     private ResourceInfo resourceInfo;
 
-    private static final String AUTHORIZATION_PROPERTY = "Authorization";
-
     @Override
     public void filter(ContainerRequestContext requestContext) {
-        Method method = resourceInfo.getResourceMethod();
+        Map<String, Cookie> cookies = requestContext.getCookies();
 
-        if(method.isAnnotationPresent(DenyAll.class)) {
-            requestContext.abortWith(Response.status(Response.Status.FORBIDDEN)
-                    .entity("Access blocked for all users !!").build());
-        } else if (!method.isAnnotationPresent(PermitAll.class) &&
-                method.isAnnotationPresent(RolesAllowed.class)) {
-            RolesAllowed rolesAnnotation = method.getAnnotation(RolesAllowed.class);
-            Set<String> requiredRoles = new HashSet<>(Arrays.asList(rolesAnnotation.value()));
+        Cookie cookie = null;
 
-            String userRole = getToken(requestContext.getHeaders());
-
-            if (userRole == null || !isUserAllowed(requiredRoles, userRole)){
-                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
-                        .entity("You cannot access this resource").build());
-            }
+        for (Map.Entry<String, Cookie> entry : cookies.entrySet()) {
+            if (entry.getKey().equals("logincookie")){
+                cookie = entry.getValue();
+            };
         }
 
 
-    }
 
-    /**
-     * checks if the userrole is sufficent to access the service
-     * @param requiredRoles  the required roles
-     * @param userRole  the role of this user
-     * @return  allowed true/false
-     */
-    private boolean isUserAllowed(final Set<String> requiredRoles, String userRole)
-    {
-        return (requiredRoles.contains(userRole));
-    }
+        Method called = resourceInfo.getResourceMethod();
 
-    /**
-     * get the authorization token
-     * @param headers  the request headers
-     * @return token
-     */
-    private String getToken(MultivaluedMap<String, String> headers) {
-        String token = "";
-        final List<String> authorizations = headers.get(AUTHORIZATION_PROPERTY);
-        if (authorizations != null) {
-            for (String entry : authorizations) {
-                String[] data = entry.split(" ");
-                if (data[0].equals("Bearer")) {
-                    token = data[1];
-                }
-            }
 
-            final Map<String, String> claims = JWToken.readClaims(token);
-            if (claims.isEmpty()) return null;
-            return claims.get("role");
-        } else {
-            return null;
+
+        if (called.isAnnotationPresent(DenyAll.class)) {
+            requestContext.abortWith(Response.status(UNAUTHORIZED).build());
+            return;
         }
+
+        if (called.isAnnotationPresent(PermitAll.class)) {
+            return;
+        }
+
+        if (cookie == null) {
+            requestContext.abortWith(Response.status(UNAUTHORIZED).build());
+            return;
+        }
+
+        String loggedIn = AES256.decrypt(cookie.getValue()).split(";")[1];
+
+        if (called.isAnnotationPresent(RolesAllowed.class)) {
+            String[] roles = called.getAnnotation(RolesAllowed.class).value();
+
+            if (Arrays.stream(roles).noneMatch(r -> r.equals(loggedIn))) {
+                requestContext.abortWith(Response.status(UNAUTHORIZED).build());
+            }
+            return;
+        }
+
+
+
+        requestContext.abortWith(Response.status(UNAUTHORIZED).entity("Requested resource doesn't have any access restrictions.").build());
+
+
     }
+
 }
